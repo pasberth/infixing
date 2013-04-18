2103eb2205c79ae6112ac244efcb6b3f195d3466
pasberth <pasberth@gmail.com>
2013-04-19 00:40:54 +0900

    added tests

--- test/infixing/core_test.clj
+++ test/infixing/core_test.clj
@@ -2,7 +2,7 @@
   (:use clojure.test
         infixing.core))
 
-(def eq-rule    (infix 4 '=))
+(def eq-rule    (infix  4 '=))
 (def add-rule   (infixl 6 '+))
 (def sub-rule   (infixl 6 '-))
 (def mul-rule   (infixl 7 '*))
@@ -17,9 +17,21 @@
   (testing "(x + y + z) == (+ (+ x y) z)"
     (is (= (infixing add-rule '(x + y + z)) '(+ (+ x y) z)))))
 
+(deftest add-mul-add-rule-test
+  (testing "(a + b * c + d) == (+ (+ a (* b c)) d)"
+    (is (= (infixing (rules add-rule mul-rule) '(a + b * c + d)) '(+ (+ a (* b c)) d)))))
+
+(deftest mul-add-mul-rule-test
+  (testing "(a * b + c * d) == (+ (* a b) (* c d))"
+    (is (= (infixing (rules add-rule mul-rule) '(a * b + c * d)) '(+ (* a b) (* c d))))))
+
 (deftest and-rule-test1
   (testing "(x and y) == (and x y)"
     (is (= (infixing and-rule '(x and y)) '(and x y)))))
 (deftest and-rule-test2
   (testing "(x and y and z) == (and x (and y z))"
     (is (= (infixing and-rule '(x and y and z)) '(and x (and y z))))))
+
+(deftest eq-and-eq-rule-test
+  (testing "(a = b and c = d) == (and (= a b) (= c d))"
+    (is (= (infixing (rules eq-rule and-rule) '(a = b and c = d)) '(and (= a b) (= c d))))))
