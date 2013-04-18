b2e4cc0e80822984575d63def6cdf39ce201f0d9
pasberth <pasberth@gmail.com>
2013-04-18 13:45:38 +0900

    いろいろルール追加

--- src/infixing/control/prelude.clj
+++ src/infixing/control/prelude.clj
@@ -1,5 +1,5 @@
 (ns infixing.control.prelude
-  (:require [infixing.core :as core]))
+  (:use [infixing.core]))
 
 (defn $ [f x]
   (f x))
@@ -7,6 +7,24 @@
 (defmacro where [a b]
   `(let ~b ~a))
 
-(def $-rule     (core/infixr 0 '$))
-(def where-rule (core/infix -1 'where))
-(def ->-rule    (core/infixl 1 '->))
+(def $-rule     (infixr 0 '$))
+(def where-rule (infix -1 'where))
+(def ->-rule    (infixl 1 '->))
+(def eq-rule    (infix 4 '=))
+(def add-rule   (infixl 6 '+))
+(def sub-rule   (infixl 6 '-))
+(def mul-rule   (infixl 7 '*))
+(def div-rule   (infixl 7 '/))
+(def and-rule   (infixr 3 'and))
+(def or-rule    (infixr 2 'or))
+
+(def rule (rules $-rule
+                 where-rule
+                 ->-rule
+                 eq-rule
+                 add-rule
+                 sub-rule
+                 mul-rule
+                 div-rule
+                 and-rule
+                 or-rule))
