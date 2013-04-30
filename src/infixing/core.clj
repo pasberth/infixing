f55138313f5304b166c43504c9eb6838cc8a156b
pasberth <pasberth@gmail.com>
2013-04-30 21:53:46 +0900

    pointfreeeeeee

--- src/infixing/core.clj
+++ src/infixing/core.clj
@@ -1,19 +1,17 @@
 (ns infixing.core)
 
 (defn infixing [rules code]
-  (let   [ nodes #(map second (partition 2 %))
-           ret   #(reduce (fn [a b] `(~@b ~a)) (map reverse %))
-         ]
-  (letfn [ (return [code [_ left-node & stack]]
-             (ret (cons (concat code left-node) (nodes stack))))
+  (let   [ nodes  #(map second (partition 2 %))
+           ret    #(reduce (fn [a b] `(~@b ~a)) (map reverse %))
+           return #(ret (cons (concat %1 %2) (nodes %3)))
          ]
   (loop [ [ left-rule left-node & stack ] '()
           code  code
         ]
   (cond
-    (not (seq? code))  (return `(~code) `(~left-rule ~left-node ~@stack))
-    (empty? code)      (return code `(~left-rule ~left-node ~@stack))
-    (= 1 (count code)) (return code `(~left-rule ~left-node ~@stack))
+    (not (seq? code))  (return `(~code) left-node stack)
+    (empty? code)      (return code left-node stack)
+    (= 1 (count code)) (return code left-node stack)
     :else
       (let [ [ lft op & code ] code
              op-rule           (rules op)
@@ -24,7 +22,7 @@
         (> (op-rule :priority) (left-rule :priority))  (recur `(~op-rule (~lft ~op) ~left-rule ~left-node ~@stack) code)
         (= :right (op-rule :recur) (left-rule :recur)) (recur `(~op-rule (~lft ~op) ~left-rule ~left-node ~@stack) code)
         (= :left (op-rule :recur) (left-rule :recur))  (recur `(~op-rule ((~@(reverse left-node) ~lft) ~op) ~@stack) code)
-        :else                                          'undefined)))))))
+        :else                                          'undefined))))))
 
 (defn infix [priority symbol]
   {symbol {:priority priority :recur nil}})
