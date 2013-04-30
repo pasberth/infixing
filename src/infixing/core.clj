1a5f07e733ee1d4a1a257b2e9449c7e68181122c
pasberth <pasberth@gmail.com>
2013-05-01 00:08:05 +0900

    中置関数を任意のリストで置き換えできる機構を追加

--- src/infixing/core.clj
+++ src/infixing/core.clj
@@ -1,15 +1,14 @@
 (ns infixing.core)
 
 (defn infixing [rules code]
-  (let   [ nodes  #(map second (partition 2 %))
-           ret    #(reduce (fn [a b] `(~@b ~a)) %)
-           return #(ret (cons (concat %2 %1) (nodes %3)))
+  (let   [ ret    #(reduce (fn [a [b-rule b]] ((b-rule :repl) `(~@b ~a))) %)
+           return #(ret (cons ((%2 :repl) (concat %3 %1)) (partition 2 %4)))
          ]
   (loop [ [ left-rule left-node & stack ] '()
           code  code
         ] (cond
-    (not (seq? code))  (return `(~code) left-node stack)
-    (> 2 (count code)) (return code left-node stack)
+    (not (seq? code))  (return `(~code) left-rule left-node stack)
+    (> 2 (count code)) (return code left-rule left-node stack)
     :else
       (let [ [ lft op & code ] code
              op-rule           (rules op)
@@ -20,20 +19,20 @@
            ] (cond
         (nil? op-rule)           (recur `(~left-rule (~@left-node ~lft) ~@stack) (cons op code))
         (nil? left-rule)         (recur `(~op-rule (~op ~@left-node ~lft) ~@stack) code)
-        (< op-pr l-pr)           (recur stack `((~@left-node ~lft) ~op ~@code))
+        (< op-pr l-pr)           (recur stack `(~((left-rule :repl) `(~@left-node ~lft)) ~op ~@code))
         (or (> op-pr l-pr)
           (= :right op-rc l-rc)) (recur `(~op-rule (~op ~lft) ~left-rule ~left-node ~@stack) code)
-        (= :left  op-rc l-rc)    (recur `(~op-rule (~op (~@left-node ~lft)) ~@stack) code)
+        (= :left  op-rc l-rc)    (recur `(~op-rule (~op ~((left-rule :repl) `(~@left-node ~lft))) ~@stack) code)
         :else                    'undefined))))))
 
 (defn infix [priority symbol]
-  {symbol {:priority priority :recur nil}})
+  {symbol {:priority priority :recur nil :repl identity}})
 
 (defn infixl [priority symbol]
-  {symbol {:priority priority :recur :left}})
+  {symbol {:priority priority :recur :left :repl identity}})
 
 (defn infixr [priority symbol]
-  {symbol {:priority priority :recur :right}})
+  {symbol {:priority priority :recur :right :repl identity}})
 
 (defn rules [& rules]
   (reduce merge rules))
