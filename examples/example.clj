2d6642e236a8d2eb6c76f1f1f7e901e8a690f302
pasberth <pasberth@gmail.com>
2013-04-30 22:11:17 +0900

    代わりに examples.clj にした

---
+++ examples/example.clj
@@ -0,0 +1,17 @@
+(use 'infixing.core)
+
+(defn $ [f x]
+  (f x))
+(defmacro where [a b]
+  `(let ~b ~a))
+
+(defmacro try-infixing [code]
+  (infixing (rules (infixr 0 '$)
+                   (infix -1 'where)
+                   (infixl 1 '->))
+            code))
+
+(try-infixing (println $ (+ 1 1)))                                   ; => 2
+(try-infixing ((println x) where [x "hello world"]))                 ; => hello world
+(try-infixing (println $ (+ x x) where [x 1]))                       ; => 2
+(try-infixing (println $ 1 -> f -> g where [f #(+ 2 %) g #(+ 3 %)])) ; => 6
