dcfe7aad19e12dc5cb898dc87b08a80e055dfdcc
pasberth <pasberth@gmail.com>
2013-05-11 18:35:06 +0900

    project.clj fix

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject infixing "0.1.0"
-  :description "Infixing"
+  :description "This library provides a easy way to defining many macros of infix-operator based syntax."
   :url "https://github.com/pasberth/infixing"
-  ; :license {}
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :source-paths ["src"])
