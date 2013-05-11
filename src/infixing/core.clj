79055d5c93263bb3762ba35e44abc35bed10844c
pasberth <pasberth@gmail.com>
2013-05-11 16:54:33 +0900

    親切なエラーを表示するようにした。

--- src/infixing/core.clj
+++ src/infixing/core.clj
@@ -5,12 +5,20 @@
 
 (defn infixing [rules code]
   (if (not (seq? code)) code
-  (let   [ ret    #(reduce (fn [a [b-rule b]] ((:node-map b-rule) `(~@b ~a))) %)
-           return #(ret (cons ((:node-map %2) %1) (partition 2 %3)))
-           spc-op (fn [] true)
+  (let   [ ret         #(reduce (fn [a [b-rule b]] ((:node-map b-rule) `(~@b ~a))) %)
+           return      #(ret (cons ((:node-map %2) %1) (partition 2 %3)))
+           spc-op      (fn [] true)
+           throw-error (fn [idx] (if (even? idx)
+                         (let [n (/ idx 2) op (nth code n) top (take n code) btm (drop (inc n) code)]
+                           (throw (IllegalArgumentException. (format "Unexpected operator ``%s\" in %s" op `(~@top ~(symbol (format "``%s\"" op))  ~@btm)))))
+                         (let [next-n (/ (inc idx) 2) prev-n (dec next-n)
+                               next-op (nth code next-n) prev-op (nth code prev-n)
+                               top (take prev-n code) btm (drop (inc next-n) code)]
+                           (throw (IllegalArgumentException. (format "Unexpected space between ``%s\" and ``%s\" in %s" prev-op next-op `(~@top ~(symbol (format "``%s %s\"" prev-op next-op))  ~@btm)))))))
          ]
   (loop [ [ left-rule left-node & stack ] '()
           code                            code
+          idx                             2
         ] (cond
     (> 2 (count code)) (cond (nil? left-node) code :else (return (concat left-node code) left-rule stack))
     :else
@@ -24,14 +32,14 @@
              l-l?              (and left-rule (:left-recursion? left-rule))
            ] (cond
         (nil? op-rule)       (cond
-          (:space-rule rules)  (recur `(~left-rule ~left-node ~@stack) `(~lft ~spc-op ~op ~@code))
-          :else                (throw (IllegalArgumentException.)))
-        (nil? left-rule)     (recur `(~op-rule (~op ~@left-node ~lft) ~@stack) code)
-        (< op-pr l-pr)       (recur stack `(~((:node-map left-rule) `(~@left-node ~lft)) ~op ~@code))
+          (:space-rule rules)  (recur `(~left-rule ~left-node ~@stack) `(~lft ~spc-op ~op ~@code) (- idx 1))
+          :else                (throw-error (- idx 1)))
+        (nil? left-rule)     (recur `(~op-rule (~op ~@left-node ~lft) ~@stack) code (+ 4 idx))
+        (< op-pr l-pr)       (recur stack `(~((:node-map left-rule) `(~@left-node ~lft)) ~op ~@code) idx)
         (or (> op-pr l-pr)
-          (and op-r? l-r?))  (recur `(~op-rule (~op ~lft) ~left-rule ~left-node ~@stack) code)
-        (and op-l? l-l?)     (recur `(~op-rule (~op ~((:node-map left-rule) `(~@left-node ~lft))) ~@stack) code)
-        :else                (throw (IllegalArgumentException.)))))))))
+          (and op-r? l-r?))  (recur `(~op-rule (~op ~lft) ~left-rule ~left-node ~@stack) code (+ 4 idx))
+        (and op-l? l-l?)     (recur `(~op-rule (~op ~((:node-map left-rule) `(~@left-node ~lft))) ~@stack) code (+ 4 idx))
+        :else                (throw-error idx))))))))
 
 (defn infix [priority symbol]
   (Rules. nil {symbol (Rule. priority false false identity)}))
